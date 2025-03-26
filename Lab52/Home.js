ef0885980b1a5c1aacfecd0b9c7e1bdb236ff1aa
xuanthuan16 <thuanxuan26@gmail.com>
2025-03-26 09:44:10 +0700

    Update Lab5

--- Lab52/Home.js
+++ Lab52/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, Image } from "react-native";
+import { View, TouchableOpacity, Image, Text, StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { useDispatch, useSelector } from "react-redux";
 import { setImage } from "./redux/imageSlice";
@@ -30,11 +30,51 @@ const Home = () => {
     };
 
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Button title="Chọn ảnh" onPress={pickImage} />
-            {imageUri && <Image source={{ uri: imageUri }} style={{ width: 500, height: 500 }} />}
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={pickImage}>
+                <Text style={styles.buttonText}>Chọn ảnh</Text>
+            </TouchableOpacity>
+            {imageUri && (
+                <Image source={{ uri: imageUri }} style={styles.image} />
+            )}
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f8f9fa",
+    },
+    button: {
+        backgroundColor: "#007bff",
+        paddingVertical: 12,
+        paddingHorizontal: 24,
+        borderRadius: 10,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 5,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    image: {
+        width: 300,
+        height: 300,
+        borderRadius: 15,
+        marginTop: 20,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 5,
+    },
+});
 
 export default Home;
